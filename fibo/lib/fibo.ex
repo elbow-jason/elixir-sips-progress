2e486ba67d5acf84e11388f3ae1afdf45ce0eb8b
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-30 10:37:50 -0700

    a better fibo sequence

--- fibo/lib/fibo.ex
+++ fibo/lib/fibo.ex
@@ -1,2 +1,23 @@
 defmodule Fibo do
+
+  def fib(n) do
+    fib_p(n)
+  end
+
+  defp fib_p(0), do: []
+  defp fib_p(1), do: [1]
+  defp fib_p(2), do: [1,1]
+  defp fib_p(n) when n >= 3 do
+    fib_p([2,1,1], n - 3)
+  end
+
+  defp fib_p(acc, 0) do
+    acc |> Enum.reverse
+  end
+
+  defp fib_p([one, two | tail ], n) do
+    fib_p([one+two] ++ [one, two] ++ tail, n-1)
+  end
+
+
 end
--- fibo/test/fibo_test.exs
+++ fibo/test/fibo_test.exs
@@ -1,7 +1,23 @@
 defmodule FiboTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "fib zero" do
+    assert Fibo.fib(0) == []
+  end
+
+  test "fib one" do
+    assert Fibo.fib(1) == [1]
+  end
+
+  test "fib two" do
+    assert Fibo.fib(2) == [1,1]
+  end
+
+  test "fib six" do
+    assert Fibo.fib(6) == [1,1,2,3,5,8]
+  end
+
+  test "fib 10" do
+    assert Fibo.fib(10) == [1,1,2,3,5,8,13,21,34,55]
   end
 end
