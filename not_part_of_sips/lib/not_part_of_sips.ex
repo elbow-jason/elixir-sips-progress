1e04a8c4f2c5c65e7e52bdf978ce3fb0f622bbf1
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-29 23:37:23 -0700

    started with Map.get

--- not_part_of_sips/lib/not_part_of_sips.ex
+++ not_part_of_sips/lib/not_part_of_sips.ex
@@ -1,5 +1,3 @@
 defmodule NotSip do
-  def my_list do
-    ["Jason", "Mary"]
-  end
+
 end
--- not_part_of_sips/test/not_part_of_sips_test.exs
+++ not_part_of_sips/test/not_part_of_sips_test.exs
@@ -1,28 +1,32 @@
 defmodule NotPartOfSipsTest do
   use ExUnit.Case
 
+  def my_list do
+    ["Jason", "Mary"]
+  end
+
   test "list sigil" do
-    assert ~w(Jason Mary) == NotSip.my_list
+    assert ~w(Jason Mary) == my_list
   end
 
   test "pipe the head" do
-    assert "Jason" == NotSip.my_list |> hd
+    assert "Jason" == my_list |> hd
   end
 
   test "pipe the tail" do
-    assert ["Mary"] == NotSip.my_list |> tl
+    assert ["Mary"] == my_list |> tl
   end
 
   test "pipe the tail, pipe the head" do
-    assert "Mary" == NotSip.my_list |> tl |> hd
+    assert "Mary" == my_list |> tl |> hd
   end
 
   test "pipe the List.last" do
-    assert "Mary" == NotSip.my_list |> List.last
+    assert "Mary" == my_list |> List.last
   end
 
   test "pipe the delete" do
-    assert ["Mary"] == NotSip.my_list |> List.delete("Jason")
+    assert ["Mary"] == my_list |> List.delete("Jason")
   end
 
   test "what does foldr do?" do
@@ -40,6 +44,15 @@ defmodule NotPartOfSipsTest do
   test "what does List.wrap do? puts brackets around things" do
     #there is no List.wrap/2
     assert [1] == 1 |> List.wrap
+  end
+
+  def my_map do
+    %{boy: "Jason", girl: "Mary"}
+  end
+
+  test "what does Map.get do?" do
+    assert Map.get(my_map, :boy) == "Jason"
+    assert Map.get(my_map, :girl) == "Mary"
   end
 
 end
