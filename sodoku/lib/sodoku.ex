f67450af60e793d3cb401269d95f2cc806e4f987
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-31 12:24:34 -0700

    tests for telling if a sodoku board is solved written

--- sodoku/lib/sodoku.ex
+++ sodoku/lib/sodoku.ex
@@ -1,2 +1,13 @@
 defmodule Sodoku do
+  def solved?(board) do
+    :unfinished
+  end
+
+  def rows_solved?(board) do
+    :unfinished
+  end
+
+  def cols_solved?(board) do
+    :unfinished
+  end
 end
--- sodoku/test/sodoku_test.exs
+++ sodoku/test/sodoku_test.exs
@@ -1,13 +1,15 @@
 defmodule SodokuTest do
   use ExUnit.Case
 
+  import Sodoku
+
   test "solve returns true if the board is solved" do
     board = [
       [1,2],
       [2,1]
     ]
 
-    assert solved?(board)
+    assert Sodoku.solved?(board) == true
   end
 
   test "solve returns false if the board is not solved" do
@@ -15,7 +17,41 @@ defmodule SodokuTest do
       [1,1],
       [2,1]
     ]
-    refute solved?(board)
+    assert solved?(board) == false
+  end
+
+
+  test "rows_solved? returns true when rows are solved" do
+    board = [
+      [1,2],
+      [1,2]
+    ]
+    assert rows_solved?(board) == true
+  end
+
+  test "rows_solved? returns false when rows are not solved" do
+    board = [
+      [1,1],
+      [2,2]
+    ]
+    assert rows_solved?(board) == false
+  end
+
+  test "cols_solved? returns false when cols are not solved" do
+    board = [
+      [1,2],
+      [1,2]
+    ]
+    assert rows_solved?(board) == false
+  end
+
+
+  test "cols_solved? returns true when cols are solved" do
+    board = [
+      [2,2],
+      [1,1]
+    ]
+    assert rows_solved?(board) == true
   end
 
 end
