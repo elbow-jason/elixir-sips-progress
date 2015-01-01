09f5a7a6ca06063e1fd56297e766969a7630b55d
jlgoldb2 <jlgoldb2@asu.edu>
2015-01-01 00:51:17 -0700

    corrected fizzbuzz

--- exercise-2.exs
+++ exercise-2.exs
@@ -1,7 +1,7 @@
 defmodule FizzBuzz do
   
-  def check(0, 0, x) do
-    x
+  def check(0, 0, _) do
+    "FizzBuzz"
   end
 
   def check(0, _, _) do
@@ -10,6 +10,10 @@ defmodule FizzBuzz do
 
   def check(_, 0, _) do
     "Buzz"
+  end
+
+  def check(_, _, x) do
+    Integer.to_string(x)
   end
 
 end
@@ -21,8 +25,8 @@ defmodule TestFizzBuzz do
   use ExUnit.Case
 
   test "\"FizzBuzz\" is returned when first two args are zero" do
-    result = FizzBuzz.check(0,0,"return me")
-    assert result == "return me"
+    result = FizzBuzz.check(0,0, 12)
+    assert result == "FizzBuzz"
   end
 
   test "\"Fizz\" is returned when first arg is zero" do
@@ -34,4 +38,10 @@ defmodule TestFizzBuzz do
     result = FizzBuzz.check(12, 0, "nope")
     assert result == "Buzz"
   end
+
+  test "third arg is returned when neither two first args is zero" do
+    result = FizzBuzz.check(12, 3, 4)
+    assert result == "4"
+  end
+
 end
