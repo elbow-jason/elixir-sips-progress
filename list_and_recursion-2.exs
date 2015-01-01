2ccf12f817d6716dbfb440ead4faf3c451afadbd
jlgoldb2 <jlgoldb2@asu.edu>
2015-01-01 10:29:18 -0700

    added test for words to MaxList.max

--- list_and_recursion-2.exs
+++ list_and_recursion-2.exs
@@ -24,4 +24,10 @@ defmodule TestMaxList do
     result = MaxList.max([17, 1, 2, 3, 5])
     assert result == 17
   end
+
+  test "max still gets max" do
+    list = ~w(I am a Ruby cat. Meow meow. No?)
+    result = MaxList.max(list)
+    assert result == "meow."
+  end
 end
