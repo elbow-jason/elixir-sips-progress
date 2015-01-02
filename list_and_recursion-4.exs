c81cd929e316384979354e25be1c78ca299915bf
jlgoldb2 <jlgoldb2@asu.edu>
2015-01-01 17:23:43 -0700

    added a recursive span func

--- list_and_recursion-4.exs
+++ list_and_recursion-4.exs
@@ -1,7 +1,12 @@
 defmodule MyList do
-  def span(from, to) do
+  def span_no_recurse(from, to) do
     Enum.to_list(from..to)
   end
+
+  def span(from, to) when from <= to do
+    [from] ++ [span(from+1, to)]
+  end
+
 end
 
 ExUnit.start()
@@ -10,6 +15,11 @@ defmodule TestMyList do
   use ExUnit.Case
 
   test "a list comes out" do
+    assert MyList.span_no_recurse(0, 5) == [0,1,2,3,4,5]
+  end
+
+  test "a list comes out" do
     assert MyList.span(0, 5) == [0,1,2,3,4,5]
   end
+
 end
