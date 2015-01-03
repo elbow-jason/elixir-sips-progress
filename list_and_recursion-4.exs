37caa45a800b86595dbdfc4a6ee1295156848e09
jlgoldb2 <jlgoldb2@asu.edu>
2015-01-03 10:56:24 -0700

    added a low then high func to span

--- list_and_recursion-4.exs
+++ list_and_recursion-4.exs
@@ -7,9 +7,13 @@ defmodule MyList do
     [low]
   end
 
+  def span(high, low) when high > low do
+    [high] ++ span(high - 1, low)
+  end
+
 
   def span(low, high) when low < high do
-    [low] ++ span(low + 1, high)
+    [low] ++  span(low + 1, high)
   end
 
 end
@@ -25,6 +29,10 @@ defmodule TestMyList do
 
   test "a list comes out correctly" do
     assert MyList.span(0, 5) == [0,1,2,3,4,5]
+  end
+
+  test "and backwards" do
+    assert MyList.span(5, 0) == [5,4,3,2,1,0]
   end
 
 end
