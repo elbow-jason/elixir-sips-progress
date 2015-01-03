7e2c3c6ceaeea703a37e27d00257b52f18d2d3f0
jlgoldb2 <jlgoldb2@asu.edu>
2015-01-03 12:42:47 -0700

    anagram? and is_subset? passing tests. woohoo

---
+++ strings_and_binaries-2.exs
@@ -0,0 +1,32 @@
+defmodule MyStr do
+
+  def anagram?(word1, word2) do
+    chars1 = word1 |> String.to_char_list
+    chars2 = word2 |> String.to_char_list
+    is_subset?(chars1, chars2) and is_subset?(chars2, chars1)
+  end
+
+  def is_subset?(set_one, set_two) do
+    set_one |> Enum.all?(fn ch -> ch in set_two end)
+  end
+
+end
+
+ExUnit.start()
+
+defmodule TestMyStr do
+  use ExUnit.Case
+
+  test "anagram?/2 works" do
+    assert MyStr.anagram?("jason", "asjon") == true
+    assert MyStr.anagram?("abc", "def") == false
+    assert MyStr.anagram?("not same", "not_same") == false
+  end
+
+  test "is_subset? works" do
+    assert MyStr.is_subset?('abc', 'abcd') == true
+    assert MyStr.is_subset?('abcd', 'abcd') == true
+    assert MyStr.is_subset?('abcd', 'abc') == false
+  end
+  
+end
